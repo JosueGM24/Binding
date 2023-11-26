b5788617302cde7f6778fc6e42d4c01f2c4d43f4
Daniel <jos.god.mel.10@gmail.com>
2023-11-25 22:08:55 -0600

    Agregar archivos de proyecto.

---
+++ Binding/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Binding.Models;
+using Microsoft.Maui;
+using System.Collections.ObjectModel;
+
+namespace Binding
+{
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<Persona> People = new ObservableCollection<Persona>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ShowData_Clicked(object sender, EventArgs e)
+        {
+            Persona persona = new Persona(name.Text,lastname1.Text,lastname2.Text);
+            People.Add(persona);
+            collectionPeople.ItemsSource = People;
+
+        }
+        
+    }
+}
---
+++ Binding/Models/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binding.Models
+{
+    internal class Persona
+    {
+        public string name{ get; set; }
+        public string lastname1 { get; set; }
+        public string lastname2 { get; set; }
+        public Persona(string name, string lastname1, string lastname2) 
+        {
+            this.name = name;
+            this.lastname1 = lastname1;
+            this.lastname2 = lastname2;
+        }
+    }
+}
